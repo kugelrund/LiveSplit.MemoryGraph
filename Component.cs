a5835480c3dd65065bb880a1f5dad7a39aa77fe4
Sui <link_matrix1@o2.pl>
2016-04-14 19:48:05 +0200

    Are you sonic?

--- Component.cs
+++ Component.cs
@@ -104,6 +104,7 @@ namespace LiveSplit.MemoryGraph
 
     public class Component : IComponent
     {
+        SonicHandling sonic;
         private Settings settings;
 
         public string ComponentName => "MemoryGraph";
@@ -119,9 +120,13 @@ namespace LiveSplit.MemoryGraph
         public float MinimumHeight => settings.GraphHeight + 2 * settings.VerticalMargins;
 
         public IDictionary<string, Action> ContextMenuControls => null;
+        bool firstLoad = true;
 
         private int graphHeight;
         private int graphWidth;
+        private int drawCounter = 0;                                        //For smoothing out
+        private float avaragedValue;                                    //For smoothing out
+        private float[] fake_particles;
 
         private float currentValue;
         private System.Diagnostics.Process process;
@@ -132,6 +137,8 @@ namespace LiveSplit.MemoryGraph
         private Brush graphBrush;
         private StringFormat valueTextFormat;
         private StringFormat descriptiveTextFormat;
+        private PointF[] polygon_points;
+
 
         public Component(LiveSplitState state)
         {
@@ -142,8 +149,11 @@ namespace LiveSplit.MemoryGraph
             descriptiveTextFormat.LineAlignment = StringAlignment.Center;
 
             graphBrush = Brushes.Transparent;
+            polygon_points = new PointF[4] { new Point(0, 0), new Point(0, 0), new Point(0, 0), new Point(0, 0), };  //LB, LU, RU, RB
+            fake_particles = new float[5] { 185, 143, 6, 168, 91 };
 
             settings = new Settings();
+            sonic = new SonicHandling();
             settings.HandleDestroyed += SettingsUpdated;
             SettingsUpdated(null, null);
         }
@@ -178,6 +188,11 @@ namespace LiveSplit.MemoryGraph
 
         public void DrawGraph(Graphics g, LiveSplitState state, float width, float height)
         {
+            if(firstLoad)
+            {
+                SettingsUpdated(null, null);
+                firstLoad = false;
+            }
             // figure out where to draw the graph
             RectangleF graphRect = new RectangleF();
             graphRect.Y = (height - graphHeight) / 2;
@@ -278,7 +293,129 @@ namespace LiveSplit.MemoryGraph
                         g.FillRectangle(graphBrush, barRect);
                     }
                     break;
-            }      
+
+                case GraphStyle.Polygonal:
+                    gBuffer.DrawImageUnscaled(bmpBuffer, -1, 0);
+                    gBuffer.FillRectangle(Brushes.Transparent, graphWidth - 1, 0, 1, graphHeight);
+
+                    avaragedValue += relativeValue;
+
+                    if (drawCounter==10)
+                    {
+                        avaragedValue = avaragedValue / 10.0f;
+                        //LU, LL, RB, RU
+                        //X,Y, width, height
+                        polygon_points[0].X = graphWidth - 11;
+                        polygon_points[0].Y = polygon_points[3].Y;
+                        polygon_points[1].X = graphWidth - 11;
+                        polygon_points[1].Y = graphHeight;
+                        polygon_points[2].X = graphWidth;
+                        polygon_points[2].Y = graphHeight;
+                        polygon_points[3].X = graphWidth;
+                        if (currentValue > settings.MinimumValue)
+                            polygon_points[3].Y = graphHeight - (avaragedValue * graphHeight);
+                        else
+                            polygon_points[3].Y = graphHeight;
+                        gBuffer.FillPolygon(graphBrush, polygon_points);
+                        avaragedValue = 0;
+                        drawCounter = 0;
+                    }
+                    else
+                        drawCounter++;
+
+
+
+                    if (descriptiveNextToGraph || valueNextToGraph)
+                    {
+                        g.DrawImageUnscaled(bmpBuffer, (int)graphRect.X, (int)graphRect.Y);
+                    }
+                    else
+                    {
+                        g.DrawImage(bmpBuffer, settings.HorizontalMargins, settings.VerticalMargins,
+                                    width - 2 * settings.HorizontalMargins, height - 2 * settings.VerticalMargins);
+                    }
+                    break;
+
+                case GraphStyle.PolygonalOverflow:
+                    gBuffer.DrawImageUnscaled(bmpBuffer, -1, 0);
+                    gBuffer.FillRectangle(Brushes.Transparent, graphWidth - 1, 0, 1, graphHeight);
+
+
+                    //LU, LL, RB, RU
+                    //X,Y, width, height
+                    polygon_points[0].X = graphWidth - 15;
+                    polygon_points[0].Y = polygon_points[3].Y;
+                    polygon_points[1].X = graphWidth - 15;
+                    polygon_points[1].Y = graphHeight;
+                    polygon_points[2].X = graphWidth;
+                    polygon_points[2].Y = graphHeight;
+                    polygon_points[3].X = graphWidth;
+                    if (currentValue > settings.MinimumValue)
+                        polygon_points[3].Y = graphHeight - (relativeValue * graphHeight);
+                    else
+                        polygon_points[3].Y = graphHeight;
+                    gBuffer.FillPolygon(graphBrush, polygon_points);
+
+                    if (descriptiveNextToGraph || valueNextToGraph)
+                    {
+                        g.DrawImageUnscaled(bmpBuffer, (int)graphRect.X, (int)graphRect.Y);
+                    }
+                    else
+                    {
+                        g.DrawImage(bmpBuffer, settings.HorizontalMargins, settings.VerticalMargins,
+                                    width - 2 * settings.HorizontalMargins, height - 2 * settings.VerticalMargins);
+                    }
+                    break;
+
+                case GraphStyle.Sonic:
+                    avaragedValue += relativeValue;
+
+                    if (drawCounter==3)
+                    {
+                        avaragedValue = avaragedValue / 3;
+                        gBuffer.Clear(Color.Transparent);
+                        gBuffer.DrawLine(Pens.White, fake_particles[0] - 10 * avaragedValue, graphHeight * 0.44f, fake_particles[0], graphHeight * 0.44f);
+                        gBuffer.DrawLine(Pens.White, fake_particles[1] - 10 * avaragedValue, graphHeight * 0.76f, fake_particles[1], graphHeight * 0.76f);
+                        gBuffer.DrawLine(Pens.White, fake_particles[2] - 10 * avaragedValue, graphHeight * 0.67f, fake_particles[2], graphHeight * 0.67f);
+                        gBuffer.DrawLine(Pens.White, fake_particles[3] - 10 * avaragedValue, graphHeight * 0.14f, fake_particles[3], graphHeight * 0.14f);
+                        gBuffer.DrawLine(Pens.White, fake_particles[4] - 10 * avaragedValue, graphHeight * 0.33f, fake_particles[4], graphHeight * 0.33f);
+
+                        if(graphHeight>graphWidth)
+                            gBuffer.DrawImage(sonic.getBitmap(relativeValue), 0, 0, graphWidth, graphWidth*1.27f);
+                        else
+                            gBuffer.DrawImage(sonic.getBitmap(relativeValue), 0, 0, graphHeight/1.27f, graphHeight);
+                        drawCounter = 0;
+
+                        for (int i = 0; i < fake_particles.Length; i++)
+                        {
+                            fake_particles[i] = fake_particles[i] - 15 * avaragedValue;
+                            if (fake_particles[i] < 0)
+                                fake_particles[i] += graphWidth + 5;
+                        }
+                    }
+                    else
+                    {
+                        drawCounter++;
+                    }
+
+
+
+                    //LU, LL, RB, RU
+                    //X,Y, width, height
+
+                    if (descriptiveNextToGraph || valueNextToGraph)
+                    {
+                        g.DrawImageUnscaled(bmpBuffer, (int)graphRect.X, (int)graphRect.Y);
+                    }
+                    else
+                    {
+                        g.DrawImage(bmpBuffer, settings.HorizontalMargins, settings.VerticalMargins,
+                                    width - 2 * settings.HorizontalMargins, height - 2 * settings.VerticalMargins);
+                    }
+                    break;
+
+
+            }
 
             // draw descriptive text
             if (settings.DescriptiveTextPosition != Position.None)

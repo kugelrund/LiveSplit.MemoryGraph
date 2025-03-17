e935190bd9248e48cdf277907d559872ebc721d2
Sui / SuicideMachine <SuiMachine@users.noreply.github.com>
2025-03-17 15:28:49 +0100

    Method names, usings cleared

--- Downloader.cs
+++ Downloader.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using System.IO;
-using System.Net;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
+using System.Net;
 using System.Reflection;
+using System.Windows.Forms;
 
 namespace LiveSplit.MemoryGraph
 {
-    class MonkeyDownloadingXML
+	class MonkeyDownloadingXML
     {
         public bool DownloadNew()
         {
@@ -19,7 +18,7 @@ namespace LiveSplit.MemoryGraph
             string downloadedFileLocation = "";
             if (CheckIfXMLExists(uriToSource+ xmlFileName))
             {
-                result = downloadFiles(uriToSource, xmlFileName, out downloadedFileLocation);
+                result = DownloadFiles(uriToSource, xmlFileName, out downloadedFileLocation);
             }
             else
             {
@@ -42,7 +41,7 @@ namespace LiveSplit.MemoryGraph
             }
         }
 
-        private bool downloadFiles(string sourceLocation, string file, out string tempLocation)
+        private bool DownloadFiles(string sourceLocation, string file, out string tempLocation)
         {
             WebClient wbClient = new WebClient();
             wbClient.DownloadFileCompleted += WbClient_DownloadFileCompleted;
